68da6b8e5a14d717f399c18820992bdb8683d162
Juani Greco <juanigreco@gmail.com>
2024-06-17 23:58:03 -0300

    Cambios de configuración iniciales

---
+++ src/Front_Page.jsx
@@ -0,0 +1,13 @@
+import './Front_Page.css'
+
+function Front_Page() {
+    return (
+        <div>
+        <span id="nombres">Juani</span>
+        <span id="&">&</span>
+        <span id="nombres">Julieta</span>
+        </div>
+    );
+}
+
+export default Front_Page;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Front_Page from './Front_Page.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Front_Page />
+  </React.StrictMode>
 )
